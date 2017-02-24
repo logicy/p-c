6f9967d0574d4e5ba050727647a83ac49cceb7bb
sushiljainam <sushiljain1993@gmail.com>
2017-02-24 19:34:33 +0530

    working result obj

--- pc.js
+++ pc.js
@@ -2,7 +2,7 @@
 * @Author: Sushil Jain
 * @Date:   2017-02-24 11:52:20
 * @Last Modified by:   sushiljainam
-* @Last Modified time: 2017-02-24 19:17:54
+* @Last Modified time: 2017-02-24 19:34:05
 */
 
 'use strict';
@@ -36,16 +36,19 @@ var ObjPC = function (string, opts) {
 
 var ResultPC = function (len, string, opts) {
 	var res = this;
-
+	res.len = len;
+	res.string = string;
+	res.opts = opts;
+	res.arrIndex = res.arrIndex || 0;
 	return res;
 }
 
 ObjPC.permute = function(string, opts) {
-	//return ResultPC
+	//return this(filtered args)
 };
 
 ObjPC.names = function(string, opts) {
-	//return ResultPC
+	//return this(filtered args)
 };
 
 ResultPC.prototype.count = function(opts) {
