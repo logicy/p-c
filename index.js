7c2a2dae2d58bd91dc091d7dde06398b1073905b
Sushil K Jain <sushiljain1993@gmail.com>
2017-03-03 00:41:43 +0530

    implemented api 1/n

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 var express = require('express')
 var app = express()
+var pc = require('./pc')
+
+var allMethods = ['rank', 'count', 'list'];
+function produce(args){
+  var args = args || {};
+  var method = args.action || 'all';
+  var string = args.string || '';
+
+  var methods = (method=='all' || allMethods.indexOf(method)<0) ? allMethods : [method];
+
+  var output = {};
+  methods.map(function (method) {
+    output[method] = pc(string)[method]();
+  })
+  return output;
+}
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World! and sushil and kritik ... <br> Feb:update')
+  response.send(JSON.stringify(produce(request)));
 })
 
 app.listen(app.get('port'), function() {
