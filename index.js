38e923c57f17f3882f7e0655d638c7d0e2d31523
Sushil K Jain <sushiljain1993@gmail.com>
2017-03-03 00:56:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(JSON.stringify(produce(request)));
+  response.send(JSON.stringify(produce(request.body)));
 })
 
 app.listen(app.get('port'), function() {
