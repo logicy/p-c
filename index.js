44d87bfa08ece70180d969a7cbe47066658eb1e3
Sushil K Jain <sushiljain1993@gmail.com>
2017-03-03 01:35:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
+app.use(bodyParser.json()); // for parsing application/json
+
 var pc = require('./pc')
 
 var allMethods = ['rank', 'count', 'list'];
